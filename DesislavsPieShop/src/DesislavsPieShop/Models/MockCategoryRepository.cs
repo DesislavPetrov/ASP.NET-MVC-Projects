c7be7f0f2bea32f95b88962399bbf8f57fbd8f2f
Desislav Petrov <petrov_desislav@abv.bg>
2017-03-17 00:49:52 +0200

    added models classes and some temporary mock classes

---
+++ DesislavsPieShop/src/DesislavsPieShop/Models/MockCategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DesislavsPieShop.Models
+{
+    public class MockCategoryRepository : ICategoryRepository
+    {
+        public IEnumerable<Category> Categories
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}

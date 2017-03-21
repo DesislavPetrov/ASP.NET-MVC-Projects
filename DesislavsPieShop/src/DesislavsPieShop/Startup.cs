97460e213d01b3f54471f64464ce359d924c9af5
Desislav Petrov <petrov_desislav@abv.bg>
2017-03-21 23:36:41 +0200

    added view pies by categories

--- DesislavsPieShop/src/DesislavsPieShop/Startup.cs
+++ DesislavsPieShop/src/DesislavsPieShop/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Logging;
 using DesislavsPieShop.Models;
 using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Routing;
 
 namespace DesislavsPieShop
 {
@@ -43,7 +44,22 @@ namespace DesislavsPieShop
             app.UseStatusCodePages();
             app.UseStaticFiles();
             app.UseSession(); // this must be placed before UseMvcWithDefaultRoute(), otherwise it doesn't work
-            app.UseMvcWithDefaultRoute();
+
+
+            //app.UseMvcWithDefaultRoute();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                  name: "categoryfilter",
+                  template: "Pie/{action}/{category?}",
+                  defaults: new { Controller = "Pie", action = "List" });
+
+                routes.MapRoute(
+                name: "default",
+                template: "{controller=Home}/{action=Index}/{id?}");
+
+
+            });
 
             DbInitializer.Seed(app);
         }
